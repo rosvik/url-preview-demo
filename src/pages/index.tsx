79f746e9e3cacd47fc81b91209c01c2df84d88cc
Johannes Røsvik <j.rosvik@gmail.com>
2024-05-01 00:10:22 +0200

    update styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,50 +1,53 @@
 import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import { FormEvent, useState } from "react";
-
-const inter = Inter({ subsets: ["latin"] });
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
   const [input, setInput] = useState("");
   const [previewUrl, setPreviewUrl] = useState<URL>();
-  const onSubmit = (e: FormEvent) => {
-    e.preventDefault();
-    try {
-      const url = new URL(input);
-      setPreviewUrl(url);
-      setInput("");
-    } catch (error) {
-      alert("Hey! That's not a valid URL!");
-    }
-  };
 
   return (
     <>
       <Head>
-        <title>Super awesone URL preview</title>
+        <title>Super Awesome URL Preview</title>
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
-        <div className={styles.description}>
-          <h1>Super awesone URL preview</h1>
-          <form onSubmit={onSubmit}>
-            <input
-              type="url"
-              name="url"
-              placeholder="https://example.com"
-              value={input}
-              onChange={(e) => setInput(e.target.value)}
-            />
-          </form>
-          <div className={styles.urlPreview}>
-            {previewUrl && (
-              <a href={previewUrl.href} target="_blank" rel="noreferrer">
-                {previewUrl.href}
-              </a>
-            )}
-          </div>
-        </div>
+      <main className={styles.main}>
+        <h1>Super Awesome URL Preview</h1>
+        <form
+          onSubmit={(e: FormEvent) => {
+            e.preventDefault();
+            setPreviewUrl(new URL(input));
+            setInput("");
+          }}
+        >
+          <input
+            type="url"
+            name="url"
+            placeholder="https://example.com"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+        </form>
+        {previewUrl && (
+          <Link href={previewUrl.href} className={styles.urlPreview}>
+            <div className="aside">
+              {/* <Image
+                width={200}
+                height={200}
+                src={``}
+                alt="Preview"
+              /> */}
+            </div>
+            <div className="content">
+              <div>
+                <h2>{previewUrl.host}</h2>
+              </div>
+              <div>{previewUrl.href}</div>
+            </div>
+          </Link>
+        )}
       </main>
     </>
   );
