141d58bedd3bc5612badbfb3e88624364356a97c
Johannes Røsvik <j.rosvik@gmail.com>
2024-05-01 01:37:22 +0200

    update image-loader.js

--- image-loader.js
+++ image-loader.js
@@ -1,3 +1,3 @@
 export default function imageLoader({ src, width }) {
-  return `https://img.248.no/image.jpg?&url=${src}&w=${width}`;
+  return `https://img.248.no/image.jpg?url=${src}&w=${width}`;
 }
